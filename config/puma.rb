4b73a122f8e037521dbf0adee5a9208174a3eb12
Taguchi-ivi <k.taguchi@ivisystem.com>
2022-11-13 08:27:26 +0900

    config puma appName fix

--- config/puma.rb
+++ config/puma.rb
@@ -17,7 +17,7 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 #
 # port ENV.fetch("PORT") { 3000 }
 # add ec2 puma用
-bind "unix:///var/www/test_app/tmp/sockets/puma.sock"
+bind "unix:///var/www/portfolio01-app/tmp/sockets/puma.sock"
 
 # Specifies the `environment` that Puma will run in.
 #
